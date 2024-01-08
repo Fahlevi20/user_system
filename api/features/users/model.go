8a016e2acd2da2bd252968b678033ed985789a4a
Fahlevi20 <rozakalfi@gmail.com>
2024-01-08 20:21:43 +0700

    first commit

---
+++ api/features/users/model.go
@@ -0,0 +1,54 @@
+//structure data
+package Model
+
+import{
+
+}
+
+type User struct{
+	ID string 
+	First_name string
+	Last_name string
+	Password string
+	Email [] string
+	Phone string
+	// Token
+	// user_type
+	// Refresh_token
+	// Created_at
+	// Update_at
+	// User_id
+
+}
+
+type RegisterUser struct{
+	type User struct{
+		ID string 
+		First_name string
+		Last_name string
+		Password string
+		Email [] string
+		Phone string
+		// Token
+		// user_type
+		// Refresh_token
+		// Created_at
+		// Update_at
+		// User_id
+	
+	}
+
+}
+
+type UserLogin struct{
+	ID string 
+	Password string
+	Email [] string
+	// Token
+	// user_type
+	// Refresh_token
+	// Created_at
+	// Update_at
+	// User_id
+
+}
